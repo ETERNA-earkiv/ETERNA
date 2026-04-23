9668b6525903670cbf40b146e0155991dd3bf71c
tomas-fridekrans <tomas.fridekrans@whitered.se>
2026-04-23 11:21:40 +0200

    feat: add StartRedactionRequest DTO and UI_REDACTION_REASON_MANDATORY constant

---
+++ roda-common/roda-common-data/src/main/java/org/roda/core/data/v2/ip/redaction/StartRedactionRequest.java
@@ -0,0 +1,37 @@
+package org.roda.core.data.v2.ip.redaction;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class StartRedactionRequest implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  private String aipId;
+  private String representationId;
+  private String fileId;
+  private String details;
+
+  public StartRedactionRequest() {
+  }
+
+  public StartRedactionRequest(String aipId, String representationId, String fileId, String details) {
+    this.aipId = aipId;
+    this.representationId = representationId;
+    this.fileId = fileId;
+    this.details = details;
+  }
+
+  public String getAipId() { return aipId; }
+  public void setAipId(String aipId) { this.aipId = aipId; }
+
+  public String getRepresentationId() { return representationId; }
+  public void setRepresentationId(String representationId) { this.representationId = representationId; }
+
+  public String getFileId() { return fileId; }
+  public void setFileId(String fileId) { this.fileId = fileId; }
+
+  public String getDetails() { return details; }
+  public void setDetails(String details) { this.details = details; }
+}
