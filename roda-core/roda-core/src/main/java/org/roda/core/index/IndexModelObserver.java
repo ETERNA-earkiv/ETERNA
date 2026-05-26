b6878cb9f5704553b832b9876e579805603813c9
Jerry Malmström <jerry.malmstrom@whitered.se>
2026-05-26 10:29:20 +0200

    Updating -> Update

--- roda-core/roda-core/src/main/java/org/roda/core/index/IndexModelObserver.java
+++ roda-core/roda-core/src/main/java/org/roda/core/index/IndexModelObserver.java
@@ -851,7 +851,7 @@ public class IndexModelObserver implements ModelObserver {
     } catch (NotFoundException e) {
       LOGGER.debug("AIP {} already removed from storage, skipping parent METS update", aipId);
     } catch (RequestNotValidException | GenericException | AuthorizationDeniedException | IOException | IPException | JAXBException | SAXException e) {
-      LOGGER.error("Failed to updating parentAIP when removing AIP", e);
+      LOGGER.error("Failed to update parentAIP when removing AIP", e);
       ret.add(e);
     }
 
